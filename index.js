fa8512dcf1f49ef72bd376b50869f92b566d76e2
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-21 23:24:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries = 0;
+
+function totals(batteryBatches){
+  for (const alls of batteryBatches){
+    totalBatteries += alls;
+  }
+  return totalBatteries;
+}
+console.log(totals(batteryBatches));
